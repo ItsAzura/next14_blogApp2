23434ed3656e5dfa65092571d102f9951507adba
HQuang0910 <134147330+ItsAzura@users.noreply.github.com>
2024-06-14 15:51:37 +0700

    chore: sign-in form to handle form submission

--- src/actions/user.js
+++ src/actions/user.js
@@ -2,11 +2,19 @@
 import User from '@/database/models/user';
 import { connectToDatabase } from '@/database/db';
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { cookies } from 'next/headers';
 
 export const signUpUser = async (formData) => {
   await connectToDatabase();
   try {
     const { username, email, password } = formData;
+    if (!username || !email || !password) {
+      return {
+        success: false,
+        error: 'Please enter all fields',
+      };
+    }
 
     const user = await User.findOne({ email });
     if (user) {
@@ -43,6 +51,99 @@ export const signUpUser = async (formData) => {
     return {
       success: false,
       error: error.message,
+    };
+  }
+};
+
+export const signInUser = async (formData) => {
+  await connectToDatabase();
+  try {
+    const { email, password } = formData;
+    if (!email || !password) {
+      return {
+        success: false,
+        error: 'Please enter all fields',
+      };
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return {
+        success: false,
+        error: 'User does not exist',
+      };
+    }
+
+    const passwordMatch = await bcryptjs.compare(password, user.password);
+    if (!passwordMatch) {
+      return {
+        success: false,
+        error: 'Invalid password',
+      };
+    }
+
+    const createTokenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+    const Token = jwt.sign(createTokenData, process.env.JWT_SECRET, {
+      expiresIn: '1h',
+    });
+
+    const getCookies = cookies();
+    getCookies.set('next14_token', Token, {
+      httpOnly: true,
+    });
+
+    return {
+      success: true,
+      message: 'User signed in successfully',
+      data: JSON.parse(JSON.stringify(user)),
+      token: Token,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      success: false,
+      error: 'Something went wrong',
+    };
+  }
+};
+
+export const AuthUser = async () => {
+  await connectToDatabase();
+  try {
+    const getCookies = cookies();
+    const Token = getCookies.get('next14_token')?.value || '';
+    if (Token === '') {
+      return {
+        success: false,
+        error: 'User not authenticated',
+      };
+    }
+
+    const decoded = jwt.verify(Token, process.env.JWT_SECRET);
+
+    const user = await User.findById({ _id: decoded.id });
+    if (!user) {
+      return {
+        success: false,
+        error: 'User not found',
+      };
+    } else {
+      return {
+        success: true,
+        message: 'User authenticated',
+        data: JSON.parse(JSON.stringify(user)),
+      };
+    }
+  } catch (error) {
+    console.log(error);
+    return {
+      success: false,
+      error: 'Something went wrong',
+      message: 'Something went wrong! please try again',
     };
   }
 };
--- src/app/(auth)/sign-in/page.jsx
+++ src/app/(auth)/sign-in/page.jsx
@@ -1,9 +1,37 @@
+'use client';
 import React from 'react';
 import styles from './SignIn.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
+import { signInUser } from '@/actions/user';
+import { useRouter } from 'next/navigation';
 
 const SignInPage = () => {
+  const router = useRouter();
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await signInUser(formData);
+    console.log(res);
+    if (res.success) {
+      alert(res.message);
+      router.push('/');
+    } else {
+      alert(res.error);
+    }
+  };
+
+  const isFormValid = formData.email && formData.password;
+  console.log(formData);
   return (
     <div className={styles.Container}>
       <div className={styles.Container_Left}>
@@ -11,17 +39,31 @@ const SignInPage = () => {
       </div>
       <div className={styles.Container_Right}>
         <h1>Sign In</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className={styles.input}>
             <p>Email</p>
-            <input type="email" name="email" id="username" />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
           </div>
           <div className={styles.input}>
             <p>Password</p>
-            <input type="password" name="password" id="password" />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
           </div>
           <div className={styles.btn}>
-            <button type="submit">Sign In</button>
+            <button type="submit" disabled={!isFormValid}>
+              Sign In
+            </button>
           </div>
         </form>
         <Link href="/sign-up">Don't have an account? </Link>
