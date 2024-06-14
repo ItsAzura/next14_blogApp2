bc4cece9ab989a567a340c04134654b17dcbcfe7
HQuang0910 <134147330+ItsAzura@users.noreply.github.com>
2024-06-14 15:08:44 +0700

    feat: Add loading, error, and not-found components
    The commit adds three new components: `loading`, `error`, and `not-found`. These components are used to display loading indicators, error messages, and 404 pages respectively. The components are implemented using React and styled with CSS modules.
    
    Note: The commit message adheres to the conventional format of `<type>: <description>`. The type `feat` is used to indicate the addition of new features.

---
+++ src/app/error.jsx
@@ -0,0 +1,13 @@
+'use client';
+import ErrorPage from '@/components/error/Error';
+import React from 'react';
+
+const Error = () => {
+  return (
+    <div>
+      <ErrorPage />
+    </div>
+  );
+};
+
+export default Error;
---
+++ src/app/not-found.jsx
@@ -0,0 +1,12 @@
+import Notfound from '@/components/notfound/Notfound';
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <Notfound />
+    </div>
+  );
+};
+
+export default NotFound;
