e6d02a4ac2b0a5bf0b8048900b885647cefaf5a8
HQuang0910 <134147330+ItsAzura@users.noreply.github.com>
2024-06-18 13:24:09 +0700

    fix: fixing middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,22 +1,36 @@
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
+import jwt from 'jsonwebtoken';
 
 export const middleware = async (req) => {
   const path = req.nextUrl.pathname;
-  const publicPaths = path === '/sign-in' || path === '/sign-up';
+  const publicPaths = ['/sign-in', '/sign-up'];
 
   const getCookies = cookies();
   const token = getCookies.get('next14_token')?.value || '';
 
-  if (publicPaths && token !== '') {
-    return NextResponse.redirect(new URL('/', req.nextUrl));
+  if (publicPaths.includes(path)) {
+    if (token && !isTokenExpired(token)) {
+      return NextResponse.redirect(new URL('/', req.nextUrl));
+    }
+  } else {
+    if (!token || isTokenExpired(token)) {
+      return NextResponse.redirect(new URL('/sign-in', req.nextUrl));
+    }
   }
 
-  if (!publicPaths && token === '') {
-    return NextResponse.redirect(new URL('/sign-in', req.nextUrl));
-  }
+  return NextResponse.next();
 };
 
 export const config = {
-  matcher: ['/sign-in', '/sign-up'],
+  matcher: ['/', '/blogs', '/sign-in', '/sign-up'],
 };
+
+function isTokenExpired(token) {
+  try {
+    const decoded = jwt.decode(token);
+    return decoded.exp < Date.now() / 1000;
+  } catch (err) {
+    return true;
+  }
+}
